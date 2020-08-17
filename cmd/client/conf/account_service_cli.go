25fc8f2a895a024b310e704544091601a98df250
David-solly <David.solomon_@outlook.com>
2020-08-17 18:19:42 +0100

    changed import prefix to a more genral name

--- cmd/client/conf/account_service_cli.go
+++ cmd/client/conf/account_service_cli.go
@@ -2,22 +2,22 @@ package conf
 
 import (
 	pb "github.com/David-solly/gokit-microservice-starter/pkg/api/v1"
-	account_grpc "github.com/David-solly/gokit-microservice-starter/pkg/api/v1/service"
+	service "github.com/David-solly/gokit-microservice-starter/pkg/api/v1/service"
 	grpctransport "github.com/go-kit/kit/transport/grpc"
 	"google.golang.org/grpc"
 )
 
 // NewService :
 // Call the service method through the onion layer of a service
-func NewService(conn *grpc.ClientConn) account_grpc.ServiceInterface {
+func NewService(conn *grpc.ClientConn) service.ServiceInterface {
 	var ServiceEndpoint = grpctransport.NewClient(
 		conn, "v1.MicroService", "CallService",
-		account_grpc.EncodeGRPCServiceRequest,
-		account_grpc.DecodeGRPCServiceResponse,
+		service.EncodeGRPCServiceRequest,
+		service.DecodeGRPCServiceResponse,
 		pb.ServiceResponse{},
 	).Endpoint()
 
-	return account_grpc.ServiceEndpoints{
+	return service.ServiceEndpoints{
 		ServiceEndpoint: ServiceEndpoint,
 	}
 }
